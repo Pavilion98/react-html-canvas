c17777e720078b46d69ed29171dda507bbf60565
wassim <wassim.alharakeh@mubs.edu.lb>
2021-09-04 15:27:58 +0300

    changed file name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const App = () => {
-    return <div> Upload Image into Canvas Here! </div>
+    
+
+    return (
+        <div>
+            Hello World!
+        </div>
+    );
 }
 
 export default App;
